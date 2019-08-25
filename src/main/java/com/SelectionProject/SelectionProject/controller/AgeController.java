f9a3de1ce0215c26df98bc1cdf0b1317faf2eb6b
Edilia <ediliagonzalezmateus7@gmail.com>
2019-08-25 15:44:03 -0600

    DemoProjectOk

---
+++ src/main/java/com/SelectionProject/SelectionProject/controller/AgeController.java
@@ -0,0 +1,30 @@
+package com.SelectionProject.SelectionProject.controller;
+
+        import com.SelectionProject.SelectionProject.model.Age;
+        import com.SelectionProject.SelectionProject.repository.AgeRepository;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.web.bind.annotation.*;
+
+        import javax.validation.Valid;
+
+@CrossOrigin(origins = "*")
+
+@RestController
+@RequestMapping(value = "/form")
+public class AgeController {
+
+    @Autowired
+    AgeRepository ageRepository;
+
+    @GetMapping("/age/{id}")
+    public Age getAge(@PathVariable(value = "id") long id){
+        return ageRepository.findById(id);
+    }
+
+    @PostMapping("/age")
+    public Age saveAge(@RequestBody @Valid Age age) {
+        return ageRepository.save(age);
+    }
+
+
+}
---
+++ src/main/java/com/SelectionProject/SelectionProject/repository/AgeRepository.java
@@ -0,0 +1,9 @@
+package com.SelectionProject.SelectionProject.repository;
+
+import com.SelectionProject.SelectionProject.model.Age;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgeRepository extends JpaRepository<Age, Long> {
+
+    Age findById(long id);
+}
